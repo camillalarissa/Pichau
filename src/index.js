50f2d566ff9d1ff58e1533b6f6d09c2817c90041
camillalarissa <115382914+camillalarissa@users.noreply.github.com>
2023-08-17 16:27:41 -0300

    Projeto React

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import  GlobalCss from './globalCss'
+
+
+ReactDOM.render(<>
+    <App/> <GlobalCss/>
+    </>,
+    document.getElementById('root'));
+
